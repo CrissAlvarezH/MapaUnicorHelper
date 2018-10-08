4b1667c1c95d58448bc862533412570312d8e36b
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2018-10-08 18:21:11 -0500

    Agrego listener de posicion para MainActitivy
    Con el patron observador se pueden suscribir los fragmentos a los
    cambios en la posicion

--- app/src/main/java/helpers/cristian/com/mapaunicorhelper/MainActivity.java
+++ app/src/main/java/helpers/cristian/com/mapaunicorhelper/MainActivity.java
@@ -1,14 +1,30 @@
 package helpers.cristian.com.mapaunicorhelper;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
 
@@ -16,24 +32,57 @@ import helpers.cristian.com.mapaunicorhelper.fragmentos.BloquesFragment;
 import helpers.cristian.com.mapaunicorhelper.fragmentos.RutasFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private int codPermisos = 4324;
 
     private PagerAdaptador pagerAdaptador;
 
     private ViewPager viewPager;
     private TabLayout tabs;
-    private Toolbar toolbar;
+
+    private FusedLocationProviderClient clientePosiciones;
+    private LinearLayout layoutPosNoDisp;
+
+    private ArrayList<ListenerPosiciones> listenerPosiciones = new ArrayList<>();
+
+    public interface ListenerPosiciones {
+        void cambioPosicion(LatLng posicion);
+    }
+
+    public void agregarListenerPosicion(ListenerPosiciones listener){
+        this.listenerPosiciones.add(listener);
+    }
+
+    public void removerListenerPosicion(ListenerPosiciones listener){
+        this.listenerPosiciones.remove(listener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        layoutPosNoDisp = findViewById(R.id.layout_pos_no_disp);
         viewPager = findViewById(R.id.pager);
         tabs = findViewById(R.id.tabs);
-//        toolbar = findViewById(R.id.toolbar_main);
-//        setSupportActionBar(toolbar);
 
         agregarFragmentosToTabs(viewPager, tabs);
+
+        clientePosiciones = LocationServices.getFusedLocationProviderClient(this);
+
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setFastestInterval(0);
+        locationRequest.setInterval(0);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(this, "Debes conseder los permisos", Toast.LENGTH_SHORT).show();
+            validarPermisos();
+        }else{
+            clientePosiciones.requestLocationUpdates(locationRequest, new PosicionesCallback(), null);
+        }
     }
 
     // =============================================================================================
@@ -85,4 +134,60 @@ public class MainActivity extends AppCompatActivity {
     // [ FIN ] CONFIGUTACION DE LOS TABS CON LOS FRAGMENTS
     // =============================================================================================
 
+    public class PosicionesCallback extends LocationCallback {
+
+        @Override
+        public void onLocationAvailability(LocationAvailability locationAvailability) {
+            super.onLocationAvailability(locationAvailability);
+
+            if( locationAvailability.isLocationAvailable() ){
+                layoutPosNoDisp.setVisibility(View.GONE);
+            }else{
+                layoutPosNoDisp.setVisibility(View.VISIBLE);
+            }
+
+        }
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+
+            if( layoutPosNoDisp.getVisibility() == View.GONE )
+                layoutPosNoDisp.setVisibility(View.GONE);
+
+            for(ListenerPosiciones listener : listenerPosiciones){
+                listener.cambioPosicion(
+                        new LatLng(
+                                locationResult.getLastLocation().getLatitude(),
+                                locationResult.getLastLocation().getLongitude()
+                        )
+                );
+            }
+        }
+    }
+
+    public boolean validarPermisos(){
+        boolean todosConsedidos = true;
+        ArrayList<String> permisosFaltantes = new ArrayList<>();
+
+
+        boolean permisoFineLocation = ( ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED);
+
+
+        if(!permisoFineLocation){
+            todosConsedidos = false;
+            permisosFaltantes.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+
+        if(!todosConsedidos){
+            String [] permisosArray = new String[permisosFaltantes.size()];
+            permisosArray = permisosFaltantes.toArray(permisosArray);
+
+            ActivityCompat.requestPermissions(this, permisosArray, codPermisos);
+        }
+
+        return todosConsedidos;
+    }
 }
--- app/src/main/java/helpers/cristian/com/mapaunicorhelper/fragmentos/RutasFragment.java
+++ app/src/main/java/helpers/cristian/com/mapaunicorhelper/fragmentos/RutasFragment.java
@@ -1,26 +1,48 @@
 package helpers.cristian.com.mapaunicorhelper.fragmentos;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import helpers.cristian.com.mapaunicorhelper.MainActivity;
 import helpers.cristian.com.mapaunicorhelper.R;
 
 
-public class RutasFragment extends Fragment implements OnMapReadyCallback {
+public class RutasFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener, MainActivity.ListenerPosiciones {
 
     private GoogleMap mapa;
+    private FloatingActionButton fabAddRuta, fabAddPos;
 
-    public RutasFragment() {}
+    private Marker miMarker;
+
+    public RutasFragment() {
+    }
 
 
     @Override
@@ -28,12 +50,15 @@ public class RutasFragment extends Fragment implements OnMapReadyCallback {
                              Bundle savedInstanceState) {
         View vista = inflater.inflate(R.layout.fragment_rutas, container, false);
 
-        // TODO Terminar de ajustar el icono del FAB y referenciar el XML
+        fabAddRuta = vista.findViewById(R.id.fab_add_ruta);
+        fabAddPos = vista.findViewById(R.id.fab_add_posicion_a_ruta);
 
         MapFragment mapFragment = (MapFragment) getActivity()
                 .getFragmentManager().findFragmentById(R.id.map_rutas);
 
         mapFragment.getMapAsync(this);
+
+        ( (MainActivity) getContext()).agregarListenerPosicion(this);
 
         return vista;
     }
@@ -42,5 +67,37 @@ public class RutasFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mapa = googleMap;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fab_add_ruta:
+
+                break;
+            case R.id.fab_add_posicion_a_ruta:
+
+                break;
+        }
+    }
+
+
+    @Override
+    public void cambioPosicion(LatLng posicion) {
+        if(miMarker != null){
+            miMarker.setPosition(posicion);
+            return;
+        }
+
+        miMarker = mapa.addMarker( new MarkerOptions().title("Mi posición").position(posicion) );
+
+        mapa.animateCamera( CameraUpdateFactory.newLatLngZoom(posicion, 15) );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        ( (MainActivity) getContext()).removerListenerPosicion(this);
     }
 }
